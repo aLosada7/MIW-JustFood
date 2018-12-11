85b77e614f71c936d42d419091cf875c0191c226
Alvaro Losada <aldc30sc@gmail.com>
2018-12-11 13:47:05 +0100

    Changes made before starting final version

---
+++ entregaVersion1/app.js
@@ -0,0 +1,123 @@
+// Módulos
+var express = require('express');
+var app = express();
+
+//var fs = require('fs');
+//var https = require('https');
+var expressSession = require('express-session');
+app.use(expressSession({//interesante cuanto tiempo tarda en expirar la sesión
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+var crypto = require('crypto');
+var fileUpload = require('express-fileupload');
+app.use(fileUpload());
+var mongo = require('mongodb');
+var swig  = require('swig');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+var expressSanitizer = require('express-sanitizer');
+app.use(expressSanitizer());
+
+//Routers:
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("Router usuario")
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.use(express.static('public'));
+
+//Aplicar routerUsuarioSession
+app.use("/restaurante/comprar",routerUsuarioSession);
+app.use("/pedidos",routerUsuarioSession);
+
+//routerUsuarioRestaurante
+var routerUsuarioRestaurante = express.Router();
+routerUsuarioRestaurante.use(function(req, res, next) {
+    console.log("Router Restaurant")
+    if(req.session.usuario != null) {
+        gestorBD.obtenerUsuarios(
+            { email : req.session.usuario }, function (usuarios) {
+                console.log(usuarios)
+                if(usuarios[0].tipoUsuario == "Restaurante"){
+                    next();
+                } else {
+                    res.redirect("/restaurantes?mensaje=Solamente los restaurante tienen acceso a este área");
+                }
+            })
+    } else {
+        res.redirect("/login")
+    }
+
+});
+
+//Aplicar routerUsuarioAutor
+app.use("/menus", routerUsuarioRestaurante);
+
+//routerUsuarioAutor
+var routerUsuarioAutor = express.Router();
+routerUsuarioAutor.use(function(req, res, next) {
+
+    console.log("Router autor")
+
+    var path = require('path');
+    var id = path.basename(req.originalUrl);
+    // Cuidado porque req.params no funciona
+    // en el router si los params van en la URL.
+
+    gestorBD.obtenerMenu(
+        { _id : mongo.ObjectID(id) }, function (menus) {
+            if(menus[0].restaurante == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/menus");
+            }
+        })
+
+});
+
+//Aplicar routerUsuarioAutor
+app.use("/menu/modificar",routerUsuarioAutor);
+app.use("/menu/eliminar",routerUsuarioAutor);
+
+
+// Variables
+app.set('port', process.env.PORT || 8088);
+app.set('db','mongodb://admin2018:admin2018@ds223063.mlab.com:23063/justfood');
+app.set('clave','9bBmJOP3yGfo1QB1LtSO');
+app.set('crypto',crypto);
+
+//Rutas/controladores por logica
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rrestaurantes.js")(app, swig, gestorBD);
+
+
+app.get('/', function (req, res) {
+    res.redirect('/restaurantes');
+});
+
+app.use( function (err, req, res, next ) {
+    console.log("Error producido: " + err); //we log the error in our db
+    if (! res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+});
---
+++ entregaVersion1/routes/rrestaurantes.js
@@ -0,0 +1,246 @@
+module.exports = function(app,swig,gestorBD) {
+
+    app.get('/menus/agregar', function (req, res) {
+        var respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+
+    app.post("/menu", function(req, res) {
+
+        var menu = {
+            nombre : req.sanitize(req.body.nombre),
+            primerPlato : req.sanitize(req.body.primerPlato),
+            segundoPlato : req.sanitize(req.body.segundoPlato),
+            tercerPlato: req.sanitize(req.body.tercerPlato),
+            postre: req.sanitize(req.body.postre),
+            precio: req.sanitize(req.body.precio),
+            restaurante: req.session.usuario
+        }
+
+
+        // Conectarse
+        gestorBD.insertarMenu(menu, function(id) {
+            if (id == null) {
+                res.redirect("/menus" +
+                    "?mensaje=Error al dar de alta el nuevo menú"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                res.redirect("/menus" +
+                    "?mensaje=Menú insertado correctamente"+
+                    "&tipoMensaje=alert-success ");
+            }
+        });
+
+    });
+
+    app.get("/restaurantes", function(req, res) {
+        var criterio = {"tipoUsuario": "Restaurante"};
+
+        if( req.query.busqueda != null ){
+            criterio = { "name" : req.query.busqueda};
+        }
+
+        if( req.query.ciudad != null && req.query.speciality != null){
+            criterio = { "city" : req.query.ciudad,
+                        "speciality" : req.query.speciality};
+        }
+
+        var pg = parseInt(req.query.pg); // Es String !!!
+        if ( req.query.pg == null){ // Puede no venir el param
+            pg = 1;
+        }
+
+        gestorBD.obtenerRestaurantesPg(criterio, pg , function(restaurantes, total ) {
+            if (restaurantes == null) {
+                res.redirect("/restaurantes" +
+                    "?mensaje=No hemos podido obtener los restaurantes"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+
+                var pgUltima = total/6;
+                if (total % 6 > 0 ){ // Sobran decimales
+                    pgUltima = pgUltima+1;
+                }
+
+                var respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        restaurantes : restaurantes,
+                        pgActual : pg,
+                        pgUltima : pgUltima
+                    });
+                res.send(respuesta);
+            }
+        });
+
+    });
+
+    app.get("/menus", function(req, res) {
+        var criterio = { "restaurante" : req.session.usuario };
+
+        gestorBD.obtenerMenu(criterio, function(menus) {
+            if (menus == null) {
+                res.redirect("/restaurantes" +
+                    "?mensaje=No hemos podido obtener tus menús"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                var respuesta = swig.renderFile('views/bpublicaciones.html',
+                    {
+                        menus : menus
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/restaurante/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)  };
+
+        gestorBD.obtenerRestaurante(criterio,function(restaurante){
+            if ( restaurante == null ){
+                res.send(respuesta);
+            } else {
+                criterio = { "restaurante" : restaurante[0].email };
+                gestorBD.obtenerMenu(criterio,function(menus){
+                    if ( menus == null ){
+                        res.redirect("/restaurantes" +
+                            "?mensaje=Se ha producido un error al obtener los datos del restaurante"+
+                            "&tipoMensaje=alert-danger ");
+                    } else {
+                        var respuesta = swig.renderFile('views/brestaurante.html',
+                            {
+                                restaurante : restaurante[0],
+                                menus: menus
+                            });
+                        res.send(respuesta);
+                    }
+                });
+            }
+        });
+    });
+
+
+    app.get('/menu/modificar/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)  };
+
+        gestorBD.obtenerMenu(criterio,function(menus){
+            if ( menus == null ){
+                res.redirect("/menus" +
+                    "?mensaje=Se ha producido un error al obtener los datos del menú seleccionado"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                var respuesta = swig.renderFile('views/bmenuModificar.html',
+                    {
+                        menu : menus[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
+
+    app.get('/menu/eliminar/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)  };
+
+        gestorBD.eliminarMenu(criterio,function(menus){
+            if ( menus == null ){
+                res.redirect("/menus" +
+                    "?mensaje=Se ha producido un error al obtener los datos del menú seleccionado"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                res.redirect("/menus"+
+                    "?mensaje=Menú eliminado correctamente"+
+                    "&tipoMensaje=alert-success ");
+            }
+        });
+    })
+
+    app.get('/restaurante/comprar/:id', function (req, res) {
+        var menuId = gestorBD.mongo.ObjectID(req.params.id);
+
+        gestorBD.obtenerMenu({ _id : gestorBD.mongo.ObjectID(req.params.id) }, function (menus) {
+            if(menus == null){
+                res.redirect("/restaurantes"+
+                    "?mensaje=Se ha producido un error durante la tramitación del pedido"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                var criterio = {"email" : menus[0].restaurante}
+                gestorBD.obtenerRestaurante(criterio,function(restaurante) {
+                    if(restaurante == null) {
+                        res.redirect("/restaurantes" +
+                            "?mensaje=Se ha producido un error durante la tramitación del pedido" +
+                            "&tipoMensaje=alert-danger ");
+                    } else {
+                        var compra = {
+                            usuario : req.session.usuario,
+                            restaurante: restaurante[0].name,
+                            menu : menus[0].nombre,
+                            fecha: (new Date()).toDateString()
+                        }
+
+                        gestorBD.insertarCompra(compra ,function(idPedido){
+                            if ( idPedido == null ){
+                                res.redirect("/restaurantes"+
+                                    "?mensaje=Se ha producido un error durante la tramitación del pedido"+
+                                    "&tipoMensaje=alert-danger ");
+                            } else {
+                                res.redirect("/restaurantes"+
+                                    "?mensaje=Pedido realizado con éxito"+
+                                    "&tipoMensaje=alert-success ");
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    })
+
+    app.get('/pedidos', function (req, res) {
+        var criterio = { "usuario" : req.session.usuario };
+
+        gestorBD.obtenerPedidos(criterio ,function(pedidos){
+            if (pedidos == null) {
+                res.send("Error al listar ");
+            } else {
+                var respuesta = swig.renderFile('views/bcompras.html',
+                    {
+                    pedidos: pedidos
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
+
+    app.post('/menu/modificar/:id', function (req, res) {
+
+        var id = req.params.id;
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(id)  };
+
+        var menu = {
+            nombre : req.sanitize(req.body.nombre),
+            primerPlato : req.sanitize(req.body.primerPlato),
+            segundoPlato : req.sanitize(req.body.segundoPlato),
+            tercerPlato: req.sanitize(req.body.tercerPlato),
+            postre: req.sanitize(req.body.postre),
+            precio: req.sanitize(req.body.precio),
+            restaurante: req.session.usuario
+        }
+
+        gestorBD.modificarMenu(criterio, menu, function(result) {
+            if (result == null) {
+                res.send("Error al modificar ");
+            } else {
+                    if( result == null){
+                        res.redirect("/menus"+
+                            "?mensaje=Se ha producido un error al intentar modificar el menú"+
+                            "&tipoMensaje=alert-alert ");
+                    } else {
+                        res.redirect("/menus"+
+                            "?mensaje=Menú modificado correctamente"+
+                            "&tipoMensaje=alert-success ");
+                    }
+            }
+        });
+
+    })
+};
